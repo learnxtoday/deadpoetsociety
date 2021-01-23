c69c1e1abb88aa23c7bea2511df53b08b9e3d4ec
Parth Krishna <thenerdsuperuser@gmail.com>
2021-01-23 21:18:33 +0530

    Added custom members URL routes.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,20 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+'''
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="thenerdsuperuser",
+    password="asdfghjkl;'",
+    hostname="thenerdsuperuser.mysql.pythonanywhere-services.com",
+    databasename="thenerdsuperuser$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+'''
 comments = []
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,6 +25,18 @@ def index():
     comments.append(request.form["contents"])
     return redirect(url_for('index'))
 
-@app.route('/wibble')
-def wibble():
-    return 'This is my pointless new page'
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    name = name
+    #return "Welcome " + name
+    return render_template(
+            'members.html',name=name
+        )
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=42069)
