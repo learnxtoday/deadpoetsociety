431ae7ed52e9bbf44becb51404d736f750236396
Parth Krishna <thenerdsuperuser@gmail.com>
2021-01-29 13:17:02 +0530

    Added Secret Key

--- quotinator.py
+++ quotinator.py
@@ -2,8 +2,11 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 from random import randint
 import os
 
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.secret_key = os.urandom(12)
+
 
 @app.route('/')
 def home():
@@ -53,6 +56,7 @@ def logout():
     session['logged_in'] = False
     return home()
 
+'''
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
+'''
